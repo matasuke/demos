0144c14aab1d1aa31ae6579c1346a566ef45746a
Kosuke Futamata <matasuke.f@gmail.com>
2021-03-26 16:10:06 +0900

    init

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/homepage.tsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
+import Listing from "@lekoarts/gatsby-theme-minimal-blog/src/components/listing"
+import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata"
+
+type PostsProps = {
+  posts: {
+    slug: string
+    title: string
+    date: string
+    excerpt: string
+    description: string
+    timeToRead?: number
+    tags?: {
+      name: string
+      slug: string
+    }[]
+  }[]
+  [key: string]: any
+}
+
+const Homepage = ({ posts }: PostsProps) => {
+  const { basePath, blogPath } = useMinimalBlogConfig()
+  const { siteTitle } = useSiteMetadata()
+
+  return (
+    <Layout>
+      <Listing posts={posts} showTags={false} />
+    </Layout>
+  )
+}
+
+export default Homepage
